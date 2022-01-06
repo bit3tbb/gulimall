bdf349249f1600e78b31db1c497133b649d15385
bit3tbb <bit3tbb@gmail.com>
2022-01-06 11:40:27 -0600

    product module update 2

---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/dao/SkuSaleAttrValueDao.java
@@ -0,0 +1,17 @@
+package com.atguigu.gulimall.product.dao;
+
+import com.atguigu.gulimall.product.entity.SkuSaleAttrValueEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * sku销售属性&值
+ * 
+ * @author bit3tbb
+ * @email bit3tbb@gmail.com
+ * @date 2022-01-02 11:56:30
+ */
+@Mapper
+public interface SkuSaleAttrValueDao extends BaseMapper<SkuSaleAttrValueEntity> {
+	
+}
---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/ProductAttrValueService.java
@@ -0,0 +1,20 @@
+package com.atguigu.gulimall.product.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.atguigu.common.utils.PageUtils;
+import com.atguigu.gulimall.product.entity.ProductAttrValueEntity;
+
+import java.util.Map;
+
+/**
+ * spu属性值
+ *
+ * @author bit3tbb
+ * @email bit3tbb@gmail.com
+ * @date 2022-01-02 11:56:30
+ */
+public interface ProductAttrValueService extends IService<ProductAttrValueEntity> {
+
+    PageUtils queryPage(Map<String, Object> params);
+}
+
